a30c27271bc264afeaa09fecdece410274e10bd4
Smarandache Grigorie-Theodor <grigorie2002@yahoo.com>
2024-06-08 17:57:23 +0300

    Operatii CRUD
    CRUD

---
+++ TodoApi/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TodoApi.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string nume { get; set; } = string.Empty;
+        public string prenume { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string parola { get; set; } = string.Empty;
+        public bool? admin { get; set; }
+
+
+
+    }
+}
---
+++ TodoApi/Models/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi.Models
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+        public string? Nume { get; set; }
+        public string? Prenume { get; set; }
+        public string? Email { get; set; }
+        public string? Parola { get; set; }
+        public bool? Admin { get; set; }
+    }
+}
